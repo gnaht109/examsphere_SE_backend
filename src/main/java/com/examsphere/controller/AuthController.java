4c7062472b77fc37419661424b60b702e1044d0d
bunni236 <153516769+bunni236@users.noreply.github.com>
2026-04-07 20:24:07 +0700

    Update AuthController.java

--- src/main/java/com/examsphere/controller/AuthController.java
+++ src/main/java/com/examsphere/controller/AuthController.java
@@ -1,11 +1,6 @@
 package com.examsphere.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.examsphere.dto.request.LoginRequest;
 import com.examsphere.dto.response.ApiResponse;
 import com.examsphere.dto.response.JwtResponse;
@@ -13,12 +8,14 @@ import com.examsphere.service.AuthService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 
 @RestController
 @RequestMapping("/api/auth") 
 @CrossOrigin(origins = "http://localhost:3000")
-@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthController {
 
     AuthService authService;
