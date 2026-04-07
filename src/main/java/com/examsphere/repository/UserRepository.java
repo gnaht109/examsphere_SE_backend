2f82a4d8372bb6ce2420ef97b7e7223f51887c15
bunni236 <153516769+bunni236@users.noreply.github.com>
2026-04-07 18:27:39 +0700

    Update UserRepository.java

--- src/main/java/com/examsphere/repository/UserRepository.java
+++ src/main/java/com/examsphere/repository/UserRepository.java
@@ -7,12 +7,8 @@ import com.examsphere.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Giữ nguyên kiểu trả về User như nhóm đang để
     User findByEmail(String email); 
-    
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-    
-    // Giữ lại Optional cho Username nếu nhóm đã để thế này
     Optional<User> findByUsername(String username);
 }
