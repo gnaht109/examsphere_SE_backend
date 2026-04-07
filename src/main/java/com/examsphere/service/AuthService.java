cd09326aadf4629e6f7ad8948d9cc7703eb6f5e0
bunni236 <153516769+bunni236@users.noreply.github.com>
2026-04-07 18:38:03 +0700

    Create AuthService.java

---
+++ src/main/java/com/examsphere/service/AuthService.java
@@ -0,0 +1,47 @@
+package com.examsphere.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.examsphere.config.JwtUtils;
+import com.examsphere.dto.request.LoginRequest;
+import com.examsphere.dto.response.JwtResponse;
+import com.examsphere.exception.AppException;
+import com.examsphere.exception.ErrorCode;
+import com.examsphere.model.User;
+import com.examsphere.repository.UserRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthService {
+
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+    JwtUtils jwtUtils;
+
+    public JwtResponse login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail());
+        
+        if (user == null) {
+            throw new AppException(ErrorCode.USER_NOT_EXISTED);
+        }
+
+        boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
+        if (!authenticated) {
+            throw new AppException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        String token = jwtUtils.generateTokenFromUsername(user.getEmail());
+        return JwtResponse.builder()
+                .token(token)
+                .type("Bearer")
+                .email(user.getEmail())
+                .role(user.getRole())
+                .build();
+    }
+}
